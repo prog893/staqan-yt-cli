c374421f9d8e28419a46a756b46c43af4cbab08c
Torgayev Tamirlan <10016756+prog893@users.noreply.github.com>
2026-03-19 01:10:45 +0900

    2.0.3

--- Formula/staqan-yt.rb
+++ Formula/staqan-yt.rb
@@ -1,7 +1,7 @@
 class StaqanYt < Formula
   desc "CLI tool for managing YouTube videos and metadata using the YouTube Data API v3"
   homepage "https://github.com/prog893/staqan-yt-cli"
-  version "2.0.2"
+  version "2.0.3"
   license "MIT"
 
   # Source-based installation from GitHub repo
